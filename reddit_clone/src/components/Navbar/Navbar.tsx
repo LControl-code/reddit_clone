e7d1a5e532783f15a6d3021e27cde212713d623e
Lost Control <lcontrol2326@gmail.com>
2023-02-02 22:53:54 +0100

    Video done: 26:20

---
+++ reddit_clone/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import { Flex } from '@chakra-ui/react';
+import React from 'react';
+
+type NavbarProps = {
+  
+};
+
+const Navbar:React.FC<NavbarProps> = () => {
+  
+  return (
+    <Flex bg="white" height="44px" padding="6px 12px">
+      <Flex>Logos</Flex>
+    </Flex>
+  )
+}
+export default Navbar;
